81042586a80c1245cc1b25a0ce7de61bea33ad8b
Jcheng <jimmy.cheng.75013@gmail.com>
2022-01-27 11:28:24 +0100

    chore: refact

--- contracts/ownable.sol
+++ contracts/ownable.sol
@@ -4,21 +4,25 @@ pragma solidity ^0.8.11;
 
 contract ownable {
     address public owner;
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
 
     constructor() {
         _transferOwnership(msg.sender);
     }
 
     function transferOwnership(address newOwner) public {
-        require(owner == msg.sender, "Only the owner of the contract can transfer the ownership");
+        require(
+            owner == msg.sender,
+            "Only the owner of the contract can transfer the ownership"
+        );
         _transferOwnership(newOwner);
     }
+
     function _transferOwnership(address newOwner) internal {
         emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
-    }
-    function getOwner() public view returns (address) {
-        return owner;
     }
 }
