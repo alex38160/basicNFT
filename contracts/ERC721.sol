b5dc24df4d83f4cb0cafc72215ae962f54ce3913
Jcheng <jimmy.cheng.75013@gmail.com>
2022-01-27 09:54:42 +0100

    chore: refact of the code + add standard(ERC721) function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.11;
 
 import "./IERC721.sol";
 
-contract ERC721 is IERC721 {
-
+abstract contract ERC721 is IERC721 {
     // Mapping from token ID to owner address
     mapping(uint256 => address) internal _owners;
 
@@ -15,44 +14,81 @@ contract ERC721 is IERC721 {
     // Mapping from token ID to approved address
     mapping(uint256 => address) private _tokenApprovals;
 
-    function balanceOf(address _owner) external override view returns (uint256 _balance){
+    // Mapping from token ID to its metadatas
+    mapping(uint256 => string) internal _tokenUris;
+
+    function balanceOf(address _owner)
+        external
+        view
+        override
+        returns (uint256 _balance)
+    {
         return _balances[_owner];
     }
 
-    function ownerOf(uint256 _tokenId) external override view returns (address _owner){
+    function ownerOf(uint256 _tokenId)
+        external
+        view
+        override
+        returns (address _owner)
+    {
         return _owners[_tokenId];
     }
 
-    modifier onlyOwnerOf(uint _tokenId){
-        require(msg.sender==_owners[_tokenId],"Only the owner of the token can perform that action");
+    modifier onlyOwnerOf(uint256 _tokenId) {
+        require(
+            msg.sender == _owners[_tokenId],
+            "Only the owner of the token can perform that action"
+        );
         _;
     }
 
-    function _approve(address _to, uint256 _tokenId) internal onlyOwnerOf(_tokenId){
-        _tokenApprovals[_tokenId] = _to;
-        emit Approval(msg.sender,_to,_tokenId);
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        require(_owners[_tokenId] != address(0), "This token is not minted");
+        return _tokenUris[_tokenId];
     }
 
-    function _transfer(address _from, address _to, uint256 _tokenId) private {
-        _approve(_to,_tokenId);
+    function _approve(address _to, uint256 _tokenId)
+        internal
+        onlyOwnerOf(_tokenId)
+    {
+        _tokenApprovals[_tokenId] = _to;
+        emit Approval(msg.sender, _to, _tokenId);
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) private {
+        _approve(_to, _tokenId);
         _balances[_from] -= 1;
         _balances[_to] += 1;
         _owners[_tokenId] = _to;
-        emit Transfer(_from,_to,_tokenId);
+        emit Transfer(_from, _to, _tokenId);
     }
 
-    function transfer(address _to, uint256 _tokenId) external override {
-        _transfer(msg.sender,_to,_tokenId);
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public payable override {
+        _transfer(_from, _to, _tokenId);
+    }
+
+    function transferTo(address _to, uint256 _tokenId) public payable {
+        _transfer(msg.sender, _to, _tokenId);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public payable override {
+        _transfer(msg.sender, _to, _tokenId);
     }
 
     function approve(address to, uint256 tokenId) public override {
         _approve(to, tokenId);
-    }
-
-    function takeOwnership(uint256 _tokenId) external override{
-        require(_tokenApprovals[_tokenId] == msg.sender, "Need to be approved first");
-        _balances[_owners[_tokenId]] -= 1;
-        _balances[msg.sender] += 1;
-        _owners[_tokenId] = msg.sender;
     }
 }
--- contracts/IERC721.sol
+++ contracts/IERC721.sol
@@ -1,14 +1,55 @@
-// SPDX-License-Identifier: UNLICENSED 
+// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.11;
 
 interface IERC721 {
-  event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-  event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _tokenId
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 _tokenId
+    );
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
 
-  function balanceOf(address _owner) external view returns (uint256 _balance);
-  function ownerOf(uint256 _tokenId) external view returns (address _owner);
-  function transfer(address _to, uint256 _tokenId) external;
-  function approve(address _to, uint256 _tokenId) external;
-  function takeOwnership(uint256 _tokenId) external;
+    function balanceOf(address _owner) external view returns (uint256 _balance);
+
+    function ownerOf(uint256 _tokenId) external view returns (address _owner);
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory data
+    ) external payable;
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
+
+    function approve(address _to, uint256 _tokenId) external;
+
+    function getApproved(uint256 _tokenId) external view returns (address);
+
+    function setApprovalForAll(address _operator, bool _approved) external;
+
+    function isApprovedForAll(address _owner, address _operator)
+        external
+        view
+        returns (bool);
 }
